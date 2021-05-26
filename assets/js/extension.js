5e864003958b905108093ca21c148961526828ea
unknown <xx4016481@gmail.com>
2021-05-26 21:07:26 +0800

    test

--- assets/js/extension.js
+++ assets/js/extension.js
@@ -1,4 +1,19 @@
+// function AreaClicked(id){
+// 	$('section').fadeOut(1000);
+	
+// 	$('html, body').animate({
+// 		scrollTop: parseInt($("section").offset().top)
+// 	}, 1000);
+	
+// 	// $.get("story-01.html", function(data){
+// 	// 	$("#iframe").html(data);
+// 	// });
+// }
+
 $(function() {
     $('map').imageMapResize();
-    $('.map').maphilight()
+    $('.Mainmap').maphilight();
+
+    $('#iframe').load("stories/story-01.html");
+
 });
