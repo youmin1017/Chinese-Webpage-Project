0117eda53b67b105351c836006e90cf1b2ec1d06
unknown <xx4016481@gmail.com>
2021-05-26 23:28:46 +0800

    test-02

--- assets/js/extension.js
+++ assets/js/extension.js
@@ -1,19 +1,31 @@
-// function AreaClicked(id){
-// 	$('section').fadeOut(1000);
-	
-// 	$('html, body').animate({
-// 		scrollTop: parseInt($("section").offset().top)
-// 	}, 1000);
-	
-// 	// $.get("story-01.html", function(data){
-// 	// 	$("#iframe").html(data);
-// 	// });
-// }
+function includeHTML(StoryId){
+	if($('#story-iframe').length){
+		$('#story-iframe').remove();
+	}
+
+	$('#iframe').load( "story-" + StoryId ".html" );
+}
 
 $(function() {
     $('map').imageMapResize();
     $('.Mainmap').maphilight();
 
-    $('#iframe').load("stories/story-01.html");
+    // $('#iframe').load("stories/story-01.html");
+
+    $('area').click(function(){
+
+    	var str = $(this).attr('id');
+    	var id = str.substring(4);    // stories' id
+
+    	if( $('section').is(":visible") ){
+    		$('section').fadeOut(1000);
+    	}
+    	
+    	$('html, body').animate({
+    		scrollTop: parseInt($('section').offset().top)
+    	}, 1000); // scroll to the sory position
+ 
+    	includeHTML(id);
+    });
 
 });
