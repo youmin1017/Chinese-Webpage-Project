5bedc61848805978a537253080ee624d0e0e6a24
unknown <xx4016481@gmail.com>
2021-05-27 00:55:25 +0800

    beta-01

--- assets/js/extension.js
+++ assets/js/extension.js
@@ -10,8 +10,20 @@ function includeHTML(StoryId){
 }
 
 $(function() {
-    $('map').imageMapResize();
-    $('.Mainmap').maphilight();
+	$('map').imageMapResize();
+	$('.Mainmap').maphilight();
+
+	$(window).scroll(function() {
+    	
+    	var h = $(window).scrollTop();  // gain height realtive to top
+		
+		if ( h > $('#main-pic').offset().top ) {
+			$('.go-top').fadeIn(500);
+		}  // if h > main image show it
+		else {
+			$('.go-top').fadeOut(500);
+		}
+	});
 
 
     $('area').click(function(){
@@ -19,15 +31,23 @@ $(function() {
     	var str = $(this).attr('id');
     	var id = str.substring(4);    // stories' id
 
-    	if( $('section').is(":visible") ){
-    		$('section').fadeOut(1000);
+    	if( $('#posts').is(":visible") ){
+    		$('#posts').fadeOut(1000);
     	}
     	
     	$('html, body').animate({
-    		scrollTop: parseInt($('section').offset().top)
+    		scrollTop: parseInt($('#posts').offset().top)
     	}, 1000); // scroll to the sory position
  
     	includeHTML(id);
     });
 
+    $('#go-top').click(function(){
+    	if( $('#posts').is(":hidden") ){
+    		$('#posts').fadeIn(1000);
+    		$('#story-iframe').fadeOut(500, function(){
+    			$(this).remove();
+    		}); // remove the single story frame
+    	}
+    });
 });
